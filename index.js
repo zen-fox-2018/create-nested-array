0a7656a9fef16f1ebe99f16efa8b02c3732d98a9
Zhang <zhang@android-42520aa2424d51bc.localdomain>
2018-11-28 14:04:03 +0700

    done

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-//your code here
+function acak() {
+  let abzad = 'abcdefghijklmnopqrstuvwxyz'
+  let result =  abzad.charAt(Math.floor(Math.random()* abzad.length))
+  return result
+}
+
+// console.log(acak())
+
+function createdNestedArr(row, col) {
+  let result = []
+  for (let i = 0; i < row; i++) {
+    let temp = []
+    for (let j = 0; j < col; j++) {
+      temp.push(acak())      
+    }
+    // console.log(acak())
+    result.push(temp) 
+  }
+  return result
+}
+
+console.log(createdNestedArr(4,4));
+console.log(createdNestedArr(4,2))
