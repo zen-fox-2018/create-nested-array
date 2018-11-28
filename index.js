5568a98f85c4b20571c66932d94f138b5af1bec0
ahmad <ahmad.syukron6991@gmail.com>
2018-11-28 10:15:27 +0700

    create nested aarr done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 //your code here
+
+function nestedArray(row,col) {
+    let result = []
+    for(let i = 0; i < row; i ++){
+        let bucket = []
+        for(let j =1; j <= col; j ++){
+            keyCode = String.fromCharCode(Math.floor(Math.random()*25)+65)
+            bucket.push(keyCode.toLowerCase())
+        }
+        result.push(bucket)
+    }
+    return result
+}
+console.log(nestedArray(5,3))
+console.log(nestedArray(4,2))
