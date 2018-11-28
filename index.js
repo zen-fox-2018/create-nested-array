cce0224b615d940d89c417342dafbcbe9017d0e2
Aditya Respati <AdityaRespati>
2018-11-28 19:15:02 +0700

    added solution

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-//your code here
+//your code 
+
+function NestedArray(nRow, nCol) {
+    let arrOutput = []
+
+    for (let i = 0; i < nRow; i++) {
+        let arrRow = []
+
+        for (let j = 0; j < nCol; j++){
+            arrRow.push(String.fromCharCode(Math.floor(Math.random()*26)+65).toLowerCase())
+        }
+        arrOutput.push(arrRow)
+    }
+
+    return arrOutput
+}
+
+console.log(NestedArray(5, 3))
+
+console.log(NestedArray(4, 2))
