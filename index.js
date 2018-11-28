896926c61de114cd39360794b24f239af5a64c14
fauzan_bintang <obin.aji@gmail.com>
2018-11-28 09:54:20 +0700

    fauzan

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-//your code here
+function createNestedArray(row, col) {
+    let alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    let result = []
+    for (let i = 0; i < row; i++) {
+        let temp = []
+        for (let j = 0; j < col; j++) {
+            temp.push(alphabet[Math.floor(Math.random() * alphabet.length)])
+        }
+        result.push(temp)
+    }
+    return result
+}
+
+console.log(createNestedArray(4, 3));
