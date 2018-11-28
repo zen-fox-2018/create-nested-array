057ff83522e439929301dff5bbee426559725ee1
ShadowM8 <anton2014lenovo@gmail.com>
2018-11-28 09:50:04 +0700

    create nested array done -anton-

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-//your code here
+function nestedArray(row, col) {
+    var hasil = []
+    for (var i = 0; i < row; i ++){
+        var hasilDalam = []
+        for (var j = 0; j < col; j++){
+            var angka = Math.floor(Math.random() * (90-65) + 65)
+            var huruf = String.fromCharCode(angka)
+            var hurufKecil = huruf.toLowerCase()
+            hasilDalam.push(hurufKecil)
+        }
+        hasil.push(hasilDalam)
+    }
+    return hasil
+    
+}
+
+//testcase
+console.log(nestedArray(5,3))
+console.log(nestedArray(4,2))
