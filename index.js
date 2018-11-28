4adc1b3147227a6bab121d817b66b28586f19527
Luthfi Arfiansyah <luthfi.arfi@gmail.com>
2018-11-28 10:33:07 +0700

    initial upload

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 //your code here
+function createNestedArr (jumlahRow, jumlahCol){
+  const abjad = 'abcdefghijklmnopqrstuvwxyz';
+  var result = [];
+  var isiAbjad ='';
+  for (var i = 0; i < jumlahRow; i++) {
+    var isi = [];
+    for (var j = 0; j < jumlahCol; j++) {
+      isiAbjad += abjad.charAt(Math.floor(Math.random() * abjad.length));
+      isi.push(isiAbjad);
+      isiAbjad ='';
+    }
+    result.push(isi);
+  }
+  return result;
+}
+
+console.log(createNestedArr(5,3));
+console.log(createNestedArr(4,2));
