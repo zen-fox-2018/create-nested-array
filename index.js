94b13e47ce959da948c509f185a0bc375d66f566
taqiabdulaziz <svx.annexiv@gmail.com>
2018-11-28 10:15:50 +0700

    komit

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-//your code here
+function createNestedArr(jumlahRow, jumlahCol) {
+    let arr = []
+    let arrTemp = []
+    const alph = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+    for (let i = 0; i < jumlahRow; i++) {
+        for (let j = 0; j < jumlahCol; j++) {
+            arrTemp.push(
+                alph[Math.floor(
+                    (Math.random() * alph.length )
+                )]
+            )
+        }
+        arr.push(arrTemp)
+        arrTemp = []
+
+    }
+    return arr
+}
+
+console.log(createNestedArr(5, 3));
