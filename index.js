bf6f1007501c7186363a3950c8ce4276386797f2
ChristianHS23 <christian.sihotang23@gmail.com>
2018-11-28 10:00:49 +0700

    add function createNestedArray

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 //your code here
+function createNestedArray(row,col){
+    var output=[]
+    for(let i=0; i < row; i++){
+        let outputDalam =[]
+        for(let j=0; j < col; j++){
+            let randomString = String.fromCharCode(~~((Math.random() * 26 ) + 65))
+            outputDalam.push(randomString.toLowerCase())
+        }
+        output.push(outputDalam)
+    }
+    return output
+}
+
+console.log(createNestedArray(5,3))
