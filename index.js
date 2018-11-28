6fb2bd20134963c36ec0eb6618b79b2fa3a0c29a
rahmanhasri <rahman.hasri@gmail.com>
2018-11-28 15:00:40 +0700

    maman-nested-array

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 //your code here
+function createNestedArr(jumlahRow, jumlahCol){
+  let output = []
+  let alfabet = 'abcdefghijklmnopqrstuvwxyz'
+
+  for ( let i = 0 ; i < jumlahRow ; i++){
+    let arrDalam = []
+    for (let j = 0 ; j < jumlahCol ; j++){
+      arrDalam.push(alfabet[Math.floor(Math.random() * alfabet.length)])
+    }
+    output.push(arrDalam)
+  }
+
+  return output
+}
+
+console.log(createNestedArr(5,3))
+console.log(createNestedArr(4,2))
