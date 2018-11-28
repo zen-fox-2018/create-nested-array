a4e432c8edc8c0774de423bca05a904c2ee1afd8
patriagani <patria.gani@gmail.com>
2018-11-28 10:07:15 +0700

    nested array done

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-//your code here
+function nestedArray(rows,cols) {
+  let alphabet = "abcdefghijklmnopqrstuvwxyz"
+  let array = []
+  for (let i = 0; i < rows; i++) {
+    let innerArray = []
+    for (let j = 0; j < cols; j++) {
+      innerArray.push(alphabet[Math.floor(Math.random() * alphabet.length)])
+    }
+    array.push(innerArray)
+  }
+  return console.log(array);
+}
+
+nestedArray(4,4)
