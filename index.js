c5faffd310e9e9d9b22a317d165eea432c3dc328
Theresia Coanata <theresiacoa@DESKTOP-K5BVEFS.localdomain>
2018-11-28 09:58:35 +0700

    submit task

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-//your code here
+function createNestedArr (row, col) {
+  var result = [];
+  var chars = 'abcdefghijklmnopqrstuvwxyz';
+  for (var i = 0; i < row; i++) {
+    var arr = [];
+    for (var j = 0; j < col; j++) {
+      var randomIndex = Math.floor(Math.random() * chars.length);
+      arr.push(chars[randomIndex]);
+    }
+    result.push(arr);
+  }
+  return result;
+}
+
+console.log(createNestedArr(5,3));
+console.log(createNestedArr(4,2));
