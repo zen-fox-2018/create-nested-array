1c3afb18be5ecb256c6871f8638df1dd026f16a0
Mahdihrs <mahdiharis@outlook.com>
2018-11-28 19:47:21 +0700

    membuat nested array

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 //your code here
+function createNestedArray(jumlahRow, jumlahCol) {
+  let result = []
+  var kamus = 'abcdefghijklmnopqrstuvwxyz'
+  for (let i = 0; i < jumlahRow; i++) {
+    let resultTmp = []
+    for (let j = 0; j < jumlahCol; j++) {
+      resultTmp.push(kamus[Math.floor(Math.random() * kamus.length)])
+    }
+    result.push(resultTmp)
+  }
+  return result
+}
+
+console.log(createNestedArray(5, 3))
+console.log(createNestedArray(4, 2))
