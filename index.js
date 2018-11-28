dd10d791f41f03a7ab5862e04a5a4ad43e1efd2b
ACER <fauziaazia171097@gmail.com>
2018-11-28 03:07:50 -0500

    finish

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 //your code here
+
+function createNestedArr(row,col) {
+    let arr = []
+    const alphabeth = "abcdefghijklmnopqrstuvwxyz"
+    for (let i = 0 ; i < row ; i++) {
+        let temp = []
+        for (let j = 0 ; j < col ; j++) {
+            let Random = Math.floor(Math.random()*alphabeth.length)
+            temp.push(alphabeth[Random])
+
+        }
+        arr.push(temp)
+    }
+    return arr
+}
+
+console.log(createNestedArr(4,2))
+console.log('=====================')
+console.log(createNestedArr(5,3))
