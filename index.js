b481e61111375aab3693d386f7f9a06ac98d30f9
Celyn Susanto <celyn.vy@gmail.com>
2018-11-28 09:54:27 +0700

    create nested array done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 //your code here
+function createNestedArr(jumlahRow, jumlahCol) {
+    var alphabet = "abcdefghijklmnopqrstuvwxyz";
+    var result = []
+
+    for (var i = 0; i <= jumlahRow-1; i++) {
+        var temp = [];
+        for (var j = 0; j <= jumlahCol-1; j++) {
+            var indexForAlph = Math.round((Math.random() * 25) + 0 );
+            temp.push(alphabet[indexForAlph]);
+        }    
+        result.push(temp);   
+    }
+    return result
+}
+console.log(createNestedArr(5, 3))
+console.log(createNestedArr(4, 2))
