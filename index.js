80ca0a880fbc84576fa70226a80f60c492008d79
maria <msmariaulfa33@gmail.com>
2018-11-28 16:07:14 +0700

    submit

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 //your code here
+function createNestedArr(jumlahRow, jumlahCol){
+    //your code here
+    let row = []
+    let kamus = 'abcdefghijklmnopqrstuvwxyz'
+    for(let i=0; i<jumlahRow; i++){
+        let col = []
+        for(let j=0; j<jumlahCol; j++){
+            let k = Math.floor(Math.random()*26)
+            col.push(kamus[k])
+        }
+        row.push(col)
+    }
+    return row
+}
+
+console.log(createNestedArr(5,3))
