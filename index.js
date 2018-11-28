25e68e01d7f89e6135adce7f391443527389b9f0
rachmadza <razato.zai@gmail.com>
2018-11-28 10:20:35 +0700

    push anchor create nested array

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 //your code here
+function createNestedArr(jumlahRow, jumlahCol) {
+    let output = []
+    // let isisnya = 'abcdefghijklmnopqrstuvwxyz'
+    for (let i = 0; i < jumlahRow; i++) {
+        let box = []
+        for (let j = 0; j < jumlahCol; j++) {
+            let isinya = 'abcdefghijklmnopqrstuvwxyz'
+            let index = Math.floor(Math.random()*isinya.length)
+            box.push(isinya[index])
+            
+        }
+        output.push(box)
+    }
+
+    return output
+}
+
+console.log(createNestedArr(5,3));
+console.log(createNestedArr(4,2));
