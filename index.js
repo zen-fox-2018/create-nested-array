1474b38c7ea81bcf20114e2c97fe3ee17ebd0c0d
Januarsmad <januarsmad@gmail.com>
2018-11-28 12:35:19 +0700

    done

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 //your code here
+function nestedArray(row, col) {
+    let temp = []
+    let alfabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    for(let i = 0; i < row; i++) {
+        let inner = []
+        for(let i = 0; i < col; i++) {
+            let random = Math.floor(Math.random() * alfabet.length)
+            inner.push(alfabet[random])
+        }
+        temp.push(inner)
+    }
+    return temp
+}
+
+// console.log(nestedArray(5, 5))
+// console.log(nestedArray(3, 6))
+// console.log(nestedArray(2, 4))
